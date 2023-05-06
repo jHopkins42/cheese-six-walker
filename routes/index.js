8844f0a42611a37467a7adfa9f00ed70901ebc7e
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-05 21:41:47 -0400

    updating 404

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
+
+
+
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const apiRoutes = require("./api");
+
 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+    res.status(404).send("<h1>Sorry, the cat does not approve of your 404 error.</h1><a href='./public/derp-9.webp'> <img src='/public/derp-9.webp' alt='cat in a box'></a>");
 });
 
+
 module.exports = router;
+
